3103e3a1c8068a0221460561ba810180666e78c0
nanghaka <dndannang@gmail.com>
2015-04-19 08:21:30 +0300

    Number of Users
    This shows the number of users registered

--- user.php
+++ user.php
@@ -1,3 +1,6 @@
+<?php 
+include 'db_connect.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +9,17 @@
 <body>
 <h3>Number of Users Registered </h3>
 <p>----------------</p>
+<h6>Number of users registered </h6>
+<?php
+
+// Query the database and get the count 
+$result = mysql_query("SELECT * FROM users"); 
+$num_rows = mysql_num_rows($result); 
+
+// Display the results 
+echo $num_rows; 
+
+?>
 
 
 </body>
